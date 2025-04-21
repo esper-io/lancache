b32f9e9f19e1357db43c6e77e0f893294fa227c9
Karthik Mohan <141659498+karthikmohan-esper@users.noreply.github.com>
2025-04-21 13:26:09 +0530

    Update bootstrap.sh

--- ext/bootstrap.sh
+++ ext/bootstrap.sh
@@ -160,12 +160,12 @@ fi
 #############################################
 # 8) Esper provisioning                     #
 #############################################
-echo -e "\n${GREEN}Running Esper provisioning…${NC}"
-$SUDO bash -c "
-  curl -fsS https://artifacthub.esper.cloud/linux/scripts/dev/setup.sh -o setup.sh && \
-  chmod +x setup.sh && \
-  ./setup.sh --tenant '$TENANT' --token '$TOKEN' --blueprint '$BLUEPRINT' --group '$GROUP'
-"
+# echo -e "\n${GREEN}Running Esper provisioning…${NC}"
+# $SUDO bash -c "
+#   curl -fsS https://artifacthub.esper.cloud/linux/scripts/dev/setup.sh -o setup.sh && \
+#   chmod +x setup.sh && \
+#   ./setup.sh --tenant '$TENANT' --token '$TOKEN' --blueprint '$BLUEPRINT' --group '$GROUP'
+# "
 
 #############################################
 # 9) Fetch & version‑control python service #
